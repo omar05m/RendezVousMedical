57798fc75f970aaeddf59cced41cd29e83ed64b7
omar05m <omarkof99@gmail.com>
2024-01-17 20:51:45 +0100

    le debut de projet

---
+++ src/main/java/eheio/ma/controller/RendezVous.java
@@ -0,0 +1,24 @@
+package eheio.ma.controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RendezVous extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public RendezVous() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
