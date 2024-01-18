b134c5779cb00a7afe5ca89a3e4576f61fd5792e
omar05m <omarkof99@gmail.com>
2024-01-18 22:45:54 +0100

    fix the issues

--- src/main/java/eheio/ma/model/Assistante.java
+++ src/main/java/eheio/ma/model/Assistante.java
@@ -4,14 +4,14 @@ public class Assistante {
 	private String login;
 	private String pwd;
 	
-	public Assistante() {
-		this.login="admin";
-		this.pwd="admin";
+	public Assistante(String login,String pwd) {
+		this.login=login;
+		this.pwd=pwd;
 	}
 	
 	
 	public boolean verify() {
-		return (login==pwd);
+		return (login.equals("admin")&&pwd.equals("admin")) ? true : false;
 	}
 	
 
